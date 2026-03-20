fd3fe21bb5340b259ffb62f829045fd1b5b921c9
Shunto Ishiguro <shunto.ishiguro.job@gmail.com>
2026-03-20 19:21:11 +0900

    perf: CrokPageのChatMessage/ChatInputをlazy化してTBT改善
    kuromoji(形態素解析)+react-markdown+KaTeX+remarkプラグインを
    Crokチャンクから分離、必要時にのみロード

--- application/client/src/components/crok/CrokPage.tsx
+++ application/client/src/components/crok/CrokPage.tsx
@@ -1,7 +1,7 @@
-import { useRef } from "react";
+import { lazy, Suspense, useRef } from "react";
 
-import { ChatInput } from "@web-speed-hackathon-2026/client/src/components/crok/ChatInput";
-import { ChatMessage } from "@web-speed-hackathon-2026/client/src/components/crok/ChatMessage";
+const ChatInput = lazy(() => import("@web-speed-hackathon-2026/client/src/components/crok/ChatInput").then(m => ({ default: m.ChatInput })));
+const ChatMessage = lazy(() => import("@web-speed-hackathon-2026/client/src/components/crok/ChatMessage").then(m => ({ default: m.ChatMessage })));
 import { WelcomeScreen } from "@web-speed-hackathon-2026/client/src/components/crok/WelcomeScreen";
 import { FontAwesomeIcon } from "@web-speed-hackathon-2026/client/src/components/foundation/FontAwesomeIcon";
 import { useHasContentBelow } from "@web-speed-hackathon-2026/client/src/hooks/use_has_content_below";
@@ -27,9 +27,11 @@ export const CrokPage = ({ messages, isStreaming, onSendMessage }: Props) => {
         <div className="mx-auto max-w-2xl px-4 py-8">
           {messages.length === 0 && <WelcomeScreen />}
 
-          {messages.map((message, index) => (
-            <ChatMessage key={index} message={message} />
-          ))}
+          <Suspense fallback={null}>
+            {messages.map((message, index) => (
+              <ChatMessage key={index} message={message} />
+            ))}
+          </Suspense>
           <div ref={messagesEndRef} />
         </div>
       </div>
@@ -44,7 +46,9 @@ export const CrokPage = ({ messages, isStreaming, onSendMessage }: Props) => {
             <FontAwesomeIcon iconType="arrow-down" styleType="solid" />
           </button>
         )}
-        <ChatInput isStreaming={isStreaming} onSendMessage={onSendMessage} />
+        <Suspense fallback={null}>
+          <ChatInput isStreaming={isStreaming} onSendMessage={onSendMessage} />
+        </Suspense>
       </div>
     </div>
   );
