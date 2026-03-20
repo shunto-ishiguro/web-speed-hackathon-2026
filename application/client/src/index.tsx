d86f6f4e48d86ea2e93a386a2343044bbc2c2fc2
Shunto Ishiguro <shunto.ishiguro.job@gmail.com>
2026-03-20 18:26:49 +0900

    perf: hydrationをstartTransitionで低優先度実行してTBT改善

--- application/client/src/index.tsx
+++ application/client/src/index.tsx
@@ -2,17 +2,21 @@ import "./index.css";
 import "./buildinfo";
 
 import { hydrateRoot } from "react-dom/client";
+import { startTransition } from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router";
 
 import { AppContainer } from "@web-speed-hackathon-2026/client/src/containers/AppContainer";
 import { store } from "@web-speed-hackathon-2026/client/src/store";
 
-hydrateRoot(
-  document.getElementById("app")!,
-  <Provider store={store}>
-    <BrowserRouter>
-      <AppContainer />
-    </BrowserRouter>
-  </Provider>,
-);
+// hydrationをstartTransitionで低優先度にし、メインスレッドブロッキングを軽減
+startTransition(() => {
+  hydrateRoot(
+    document.getElementById("app")!,
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppContainer />
+      </BrowserRouter>
+    </Provider>,
+  );
+});
